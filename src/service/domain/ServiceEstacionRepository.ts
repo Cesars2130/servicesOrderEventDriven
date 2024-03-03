740d3b7f0d1aa3c9ca8b095d5d78c3dd283cf01b
Cesars2130 <cesars.213008@gmail.com>
2024-03-03 15:55:21 -0600

    .env ip elastica rabbit

--- src/service/domain/ServiceEstacionRepository.ts
+++ src/service/domain/ServiceEstacionRepository.ts
@@ -1,6 +1,5 @@
 import { ServiceEstacion } from "./ServiceEstacion";
 
 export interface ServiceEstacionRepository {
-
   createServiceEstacion(name: string, zone: string): Promise<ServiceEstacion | null>;
 }
